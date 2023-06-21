eb1c66de497af3295f407837c040769cb9eebf2b
guy the <hantn16@outlook.com>
2023-06-21 13:35:57 +0700

    4th commit

--- mysql/19-practice.sql
+++ mysql/19-practice.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE online_shop;
+-- CREATE DATABASE online_shop;
 CREATE TABLE products(
     name VARCHAR(200),
     price FLOAT,
@@ -7,18 +7,37 @@ CREATE TABLE products(
     image VARCHAR(300)
 );
 INSERT INTO products(
-    name,price,description,amount_in_stock,image
-) VALUES ('red chair',150000,'the red chair to sit',10,'/products/chair/red-chair');
+        name,
+        price,
+        description,
+        amount_in_stock,
+        image
+    )
+VALUES (
+        'red chair',
+        150000,
+        'the red chair to sit',
+        10,
+        '/products/chair/red-chair'
+    );
 INSERT INTO products(
-    name,price,description,amount_in_stock,image
-) VALUES ('blue desk',150000,'the blue desk to work',10,'/products/desk/blue-desk');
-
+        name,
+        price,
+        description,
+        amount_in_stock,
+        image
+    )
+VALUES (
+        'blue desk',
+        150000,
+        'the blue desk to work',
+        10,
+        '/products/desk/blue-desk'
+    );
 ALTER TABLE products
 MODIFY COLUMN name VARCHAR(200) NOT NULL,
-MODIFY COLUMN price FLOAT NOT NULL,
-ADD CONSTRAINT price_positive CHECK(price > 0),
-ADD CONSTRAINT amount_positive CHECK(amount_in_stock > 0);
-
-
+    MODIFY COLUMN price FLOAT NOT NULL,
+    ADD CONSTRAINT price_positive CHECK(price > 0),
+    ADD CONSTRAINT amount_positive CHECK(amount_in_stock > 0);
 ALTER TABLE products
 ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
--- postgresql/19-practice.sql
+++ postgresql/19-practice.sql
@@ -1,4 +1,45 @@
 -- CREATE DATABASE online_shop;
-CREATE TABLE products(
-    
-)
+-- CREATE TABLE products(
+--   product_name VARCHAR(200),
+--   price FLOAT,
+--   description VARCHAR(500),
+--   amount_in_stock FLOAT,
+--   image VARCHAR(200)
+-- );
+-- INSERT INTO products(
+--     product_name,
+--     price,
+--     description,
+--     amount_in_stock,
+--     image
+--   )
+-- VALUES (
+--     'red chair',
+--     150000,
+--     'the red chair to sit',
+--     10,
+--     '/products/chair/red-chair'
+--   );
+-- INSERT INTO products(
+--     product_name,
+--     price,
+--     description,
+--     amount_in_stock,
+--     image
+--   )
+-- VALUES (
+--     'blue desk',
+--     150000,
+--     'the blue desk to work',
+--     10,
+--     '/products/desk/blue-desk'
+--   );
+-- ALTER TABLE products
+-- ALTER COLUMN product_name
+-- SET DATA TYPE VARCHAR(300),
+--   ALTER COLUMN image
+-- SET DATA TYPE VARCHAR(500),
+--   ADD CONSTRAINT price_positive CHECK(price > 0),
+--   ADD CONSTRAINT amount_positive CHECK(amount_in_stock > 0);
+ALTER TABLE products
+ADD COLUMN id SERIAL PRIMARY KEY;
